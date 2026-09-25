c6babbd38545234bfbfb470d31c9f54ebaf4562f
Flip <flip014@icloud.com>
2026-09-25 17:03:06 +0300

    фикс 6 задания

--- src/lab01/ex06.py
+++ src/lab01/ex06.py
@@ -1,10 +1,10 @@
-n = int(input())
+n = int(input("in_1: "))
 a = 0
 b = 0
-for _ in range(n):
-    x = input().split()
+for i in range(n):
+    x = input(f"in_{i + 2}: ").split()
     if x[-1] == "True":
         a += 1
     else:
         b += 1
-print(a, b)
+print("out:", a, b)
